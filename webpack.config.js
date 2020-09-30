8c3d7e6fc671d331e49150c4ee78ecd92c7ab017
Jatashankar Shukla <jashukla@in.ibm.com>
2020-09-30 12:54:36 +0530

    created new index1.js and pointed web config to it

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./lib/components/Index.js",
+  entry: "./lib/components/Index1.js",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
   },
   module: {
     rules: [
-      {test: /\.js$/,exclude: /node_modules/,use: "babel-loader" }
+      {test: /\.(js|jsx)$/,exclude: /node_modules/,use: "babel-loader" }
     ]
   }
 };
